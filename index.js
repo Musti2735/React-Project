fa1e163db35f14dd33e360948d3730c5a03a358b
Mustafa Türk <58107515+Musti2735@users.noreply.github.com>
2022-03-18 16:07:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 import getData from "./app.js";
-import axios from "axios"; 
-
-//fetch("https://jsonplaceholder.typicode.com/users")
-//.then((data) => data.json())
-//.then((users) => console.log(users))
 
 
-async function Data(){
-    const {data : userId} = await ( axios("https://jsonplaceholder.typicode.com/users/1"));
-    console.log(userId)
-}
-
-Data()
+getData(1)
