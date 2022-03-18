a0ed59e0cdaf647a17bb6038767ad37fc909b0b0
Mustafa Türk <58107515+Musti2735@users.noreply.github.com>
2022-03-18 16:07:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import axios from "axios"; 
 
-async function getData(){
-    const {data : userId} = await ( axios("https://jsonplaceholder.typicode.com/users/1"));
+async function getData(idNumber){
+    const {data : userId} = await ( axios("https://jsonplaceholder.typicode.com/users/"+ idNumber));
     console.log(userId)
+    const {data : userPost} = await(axios("https://jsonplaceholder.typicode.com/posts/"+ idNumber));
+    console.log(userPost)
 }
 
 export default getData;
